196ea845bd8df8c83ff82e47e6d91f575478da2c
agungstd <apakatadunia11@gmail.com>
2025-02-19 10:58:36 +0700

    Add timestamp to API response

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,11 +3,16 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
+  timestamp: string;
 };
 
+// Handler function to respond with name and current timestamp
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ 
+    name: "Agung Priambod0", 
+    timestamp: new Date().toISOString() 
+  });
 }
